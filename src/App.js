aab51a08234e1e7b06b1b8da62878204fd63bf47
Elvis Otieno <elvisokoth365@gmail.com>
2023-03-23 20:37:28 +0300

    second commit of the day

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import CounterFunction from './react_fundamentals/usestateus';
 import Todos from './react_fundamentals/usereducer/todos';
+import Stopwatchtimmer from './react_fundamentals/use_ref/Stopwatchtimmer';
 
 function App() {
   return (
     <div className="App">      
       <CounterFunction />
       <Todos />
+      <br></br>
+      <Stopwatchtimmer />
     </div>
   );
 }
---
+++ src/react_fundamentals/use_ref/Stopwatchtimmer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Stopwatchtimmer = () => {
+  return (
+    <div>Stopwatchtimmer</div>
+  )
+}
+
+export default Stopwatchtimmer
