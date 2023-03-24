42376e4b5c344c4f7358acedda0faf3c73d17c3d
Elvis Otieno <elvisokoth365@gmail.com>
2023-03-24 08:39:50 +0300

    the third commit of 2023

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <div className="App">      
       <CounterFunction />
       <Todos />
-      <br></br>
       <Stopwatchtimmer />
     </div>
   );
--- src/react_fundamentals/use_ref/Stopwatchtimmer.js
+++ src/react_fundamentals/use_ref/Stopwatchtimmer.js
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
 const Stopwatchtimmer = () => {
+  const [name, setName] = useState('')
+  const [seconds,setSeconds] = useState(0)
+  const inputRenderCount =useRef(0)
+  const inputElement = useRef()
+  const prevName = useRef('')
+  const timerId = useRef()
+
+  function handleChange(e){
+    setName(e.target.value)
+  }
+
+  useEffect(()=>{
+    inputRenderCount.current=inputRenderCount.current+1})
+
+  function focusInput(){
+    inputElement.current.focus();
+
+  }
+  useEffect(()=>{
+    prevName.current=name
+  },[name])
+
+  function startTimer(){
+    timerId.current = setInterval(() => {
+      inputRenderCount.current ++;
+      setSeconds(prevSeconds=>prevSeconds+1);
+    }, 1000);
+  }
+
+  function stopTimer(){
+    clearInterval(timerId.current);
+    timerId.current = 0
+  }
+
+  function resetTimer(){
+    stopTimer();
+    if(seconds){
+      inputRenderCount.current++;
+      setSeconds(0)
+    }
+  }
   return (
-    <div>Stopwatchtimmer</div>
+    <>
+    <input ref={inputElement} type="text" value={name} onChange={handleChange}></input>
+    <br></br>
+    <div> My name is: {name} and it Used to be: {prevName.current}</div>
+    <br></br>
+    <span>Timer: {seconds} </span>
+    <section>
+      <button onClick={startTimer}>START</button>
+      <button onClick={stopTimer}>STOP</button>
+      <button onClick={resetTimer}>RESET</button>
+    </section>
+    <br></br>
+    <div>Rendered: {inputRenderCount.current}</div>
+    <br></br>
+    <button onClick={focusInput}>NameField</button>
+
+    </>
   )
 }
 
