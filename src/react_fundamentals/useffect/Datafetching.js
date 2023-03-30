01ec7852b0897e3a7338bd84fd1f5d83ac6af9d4
Elvis Otieno <elvisokoth365@gmail.com>
2023-03-30 17:47:25 +0300

    added useeffect hook

---
+++ src/react_fundamentals/useffect/Datafetching.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+const Datafetching = () => {
+    const [data, setData] =useState('')
+
+
+    useEffect(()=>{},[])
+  return (
+    <>
+    <p>{data}</p>
+    </>
+  )
+}
+
+export default Datafetching
