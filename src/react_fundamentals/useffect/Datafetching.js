121488e9a071982f61684e4471b4ccdf31554bbc
Elvis Otieno <elvisokoth365@gmail.com>
2023-04-02 22:39:46 +0300

    network call

--- src/react_fundamentals/useffect/Datafetching.js
+++ src/react_fundamentals/useffect/Datafetching.js
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Datafetching = () => {
     const [data, setData] =useState('')
 
 
-    useEffect(()=>{},[])
+    useEffect(()=>{
+      // Optionally the request above could also be done as
+axios.get('/user', {
+    params: {
+      ID: 12345
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+        
+    },[])
   return (
     <>
     <p>{data}</p>
